2e8daf67eab09b6a38aba3b4d5252149c84ca77c
DCHolland <Dcholla@okstate.edu>
2020-04-10 11:13:54 -0500

    added add workout page and sql dump
    Added the add workout page, and the rowers.sql dump file

---
+++ rowers.sql
@@ -0,0 +1,83 @@
+-- phpMyAdmin SQL Dump
+-- version 5.0.2
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Apr 09, 2020 at 10:07 PM
+-- Server version: 10.4.11-MariaDB
+-- PHP Version: 7.4.4
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `rowers`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `athletes`
+--
+
+CREATE TABLE `athletes` (
+  `Name` text NOT NULL,
+  `Gender` varchar(1) NOT NULL,
+  `Experience` int(11) NOT NULL,
+  `Date Joined` date NOT NULL,
+  `isCoxswain` tinyint(1) NOT NULL,
+  `CWID` int(11) NOT NULL,
+  `Dues Paid` tinyint(1) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `practices`
+--
+
+CREATE TABLE `practices` (
+  `Date` int(11) NOT NULL,
+  `Type` int(11) NOT NULL,
+  `Total Time` int(11) NOT NULL,
+  `Total Distance` int(11) NOT NULL,
+  `Average Split` int(11) NOT NULL,
+  `Average Rate` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `splits`
+--
+
+CREATE TABLE `splits` (
+  `Split Number` int(11) NOT NULL,
+  `Average Rate` int(11) NOT NULL,
+  `500 Split` int(11) NOT NULL,
+  `Distance` int(11) NOT NULL,
+  `Total Time` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `squads`
+--
+
+CREATE TABLE `squads` (
+  `Experience` text NOT NULL,
+  `Gender` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
