f6874d0aa3e4fb4cb5feb4d8d2d10e5738fac62f
micahswedbergokstate <micah.swedberg@okstate.edu>
2020-04-10 22:43:08 -0500

    Made less embarrasing
    refactored the entire program to use arrays and loops, like I probably should have from the beginning

--- insertWorkout.php
+++ insertWorkout.php
@@ -17,157 +17,58 @@
 	$athlete = $_POST['athlete'];
 	$date = $_POST['date'];
 	$type = $_POST['type'];
-	$r1 = $_POST['r1']; $s1 = $_POST['s1']; $d1 = $_POST['d1'];
-	$r2 = $_POST['r2']; $s2 = $_POST['s2']; $d2 = $_POST['d2'];
-	$r3 = $_POST['r3']; $s3 = $_POST['s3']; $d3 = $_POST['d3'];
-	$r4 = $_POST['r4']; $s4 = $_POST['s4']; $d4 = $_POST['d4'];
-	$r5 = $_POST['r5']; $s5 = $_POST['s5']; $d5 = $_POST['d5'];
-	$r6 = $_POST['r6']; $s6 = $_POST['s6']; $d6 = $_POST['d6'];
-	$r7 = $_POST['r7']; $s7 = $_POST['s7']; $d7 = $_POST['d7'];
-	$r8 = $_POST['r8']; $s8 = $_POST['s8']; $d8 = $_POST['d8'];
-	$r9 = $_POST['r9']; $s9 = $_POST['s9']; $d9 = $_POST['d9'];
-	$r10 = $_POST['r10']; $s10 = $_POST['s10']; $d10 = $_POST['d10'];
-	$r11 = $_POST['r11']; $s11 = $_POST['s11']; $d11 = $_POST['d11'];
-	$r12 = $_POST['r12']; $s12 = $_POST['s12']; $d12 = $_POST['d12'];
-	$r13 = $_POST['r13']; $s13 = $_POST['s13']; $d13 = $_POST['d13'];
-	$r14 = $_POST['r14']; $s14 = $_POST['s14']; $d14 = $_POST['d14'];
-	$r15 = $_POST['r15']; $s15 = $_POST['s15']; $d15 = $_POST['d15'];
-	$r16 = $_POST['r16']; $s16 = $_POST['s16']; $d16 = $_POST['d16'];
-	$r17 = $_POST['r17']; $s17 = $_POST['s17']; $d17 = $_POST['d17'];
-	$r18 = $_POST['r18']; $s18 = $_POST['s18']; $d18 = $_POST['d18'];
-	$r19 = $_POST['r19']; $s19 = $_POST['s19']; $d19 = $_POST['d19'];
-	$r20 = $_POST['r20']; $s20 = $_POST['s20']; $d20 = $_POST['d20'];
+	$r = Array();
+	$s = Array();
+	$d = Array();
+	$tt = Array();
+	array_push($r, $_POST['r1']);  array_push($s, $_POST['s1']);  array_push($d, $_POST['d1']);
+	array_push($r, $_POST['r2']);  array_push($s, $_POST['s2']);  array_push($d, $_POST['d2']);
+	array_push($r, $_POST['r3']);  array_push($s, $_POST['s3']);  array_push($d, $_POST['d3']);
+	array_push($r, $_POST['r4']);  array_push($s, $_POST['s4']);  array_push($d, $_POST['d4']);
+	array_push($r, $_POST['r5']);  array_push($s, $_POST['s5']);  array_push($d, $_POST['d5']);
+	array_push($r, $_POST['r6']);  array_push($s, $_POST['s6']);  array_push($d, $_POST['d6']);
+	array_push($r, $_POST['r7']);  array_push($s, $_POST['s7']);  array_push($d, $_POST['d7']);
+	array_push($r, $_POST['r8']);  array_push($s, $_POST['s8']);  array_push($d, $_POST['d8']);
+	array_push($r, $_POST['r9']);  array_push($s, $_POST['s9']);  array_push($d, $_POST['d9']);
+	array_push($r, $_POST['r10']); array_push($s, $_POST['s10']); array_push($d, $_POST['d10']);
+	array_push($r, $_POST['r11']); array_push($s, $_POST['s11']); array_push($d, $_POST['d11']);
+	array_push($r, $_POST['r12']); array_push($s, $_POST['s12']); array_push($d, $_POST['d12']);
+	array_push($r, $_POST['r13']); array_push($s, $_POST['s13']); array_push($d, $_POST['d13']);
+	array_push($r, $_POST['r14']); array_push($s, $_POST['s14']); array_push($d, $_POST['d14']);
+	array_push($r, $_POST['r15']); array_push($s, $_POST['s15']); array_push($d, $_POST['d15']);
+	array_push($r, $_POST['r16']); array_push($s, $_POST['s16']); array_push($d, $_POST['d16']);
+	array_push($r, $_POST['r17']); array_push($s, $_POST['s17']); array_push($d, $_POST['d17']);
+	array_push($r, $_POST['r18']); array_push($s, $_POST['s18']); array_push($d, $_POST['d18']);
+	array_push($r, $_POST['r19']); array_push($s, $_POST['s19']); array_push($d, $_POST['d19']);
+	array_push($r, $_POST['r20']); array_push($s, $_POST['s20']); array_push($d, $_POST['d20']);
 	$count = 0;
 	
+	//splits converted to seconds
+	function convertToSeconds($mins) {
+		return round($mins, -2)/100*60+($mins-round($mins, -2));
+	}
+
+	//calculated total time based on givens
+	function totalTime($split, $dist) {
+		return $dist/500*$split;
+	}
 	
-	//calcs based on entry
-	//$tt1 = $_GET['s1'] $_GET['d1'] ...
+	for ($i=0;$i<20;$i++) {
+		if ($d[$i]!="" && $s[$i]!="" && $r[$i]!="") {
+			$s[$i]=convertToSeconds($s[$i]);
+			$tt[$i]=totalTime($s[$i], $d[$i]);
+			$num=$i+1;
+			$sql = "INSERT INTO splits (Athlete, Date, Type, `Split Number`, `Average Rate`, `500 Split`, Distance, `Total Time`) 
+				values ('$athlete', '$date', '$type', '$num', '$r[$i]', '$s[$i]', '$d[$i]', '$tt[$i]')";
+			$result = $conn->query($sql);
+			$count++;
+		}
+	}
 	
-	//split 1
-	if ($d1 != "") {
-		$sql = "INSERT INTO splits (Athlete, Date, Type, `Split Number`, `Average Rate`, `500 Split`, Distance, `Total Time`) 
-			values ('$athlete', '$date', '$type', '1', '$r1', '$s1', '$d1', '')";
-		$result = $conn->query($sql);
-		$count++;
-	}
-	if ($d2 != "") {
-		$sql = "INSERT INTO splits (Athlete, Date, Type, `Split Number`, `Average Rate`, `500 Split`, Distance, `Total Time`) 
-			values ('$athlete', '$date', '$type', '2', '$r2', '$s2', '$d2', '')";
-		$result = $conn->query($sql);
-		$count++;
-	}
-	if ($d3 != "") {
-		$sql = "INSERT INTO splits (Athlete, Date, Type, `Split Number`, `Average Rate`, `500 Split`, Distance, `Total Time`) 
-			values ('$athlete', '$date', '$type', '3', '$r3', '$s3', '$d3', '')";
-		$result = $conn->query($sql);
-		$count++;
-	}
-	if ($d4 != "") {
-		$sql = "INSERT INTO splits (Athlete, Date, Type, `Split Number`, `Average Rate`, `500 Split`, Distance, `Total Time`) 
-			values ('$athlete', '$date', '$type', '4', '$r4', '$s4', '$d4', '')";
-		$result = $conn->query($sql);
-		$count++;
-	}
-	if ($d5 != "") {
-		$sql = "INSERT INTO splits (Athlete, Date, Type, `Split Number`, `Average Rate`, `500 Split`, Distance, `Total Time`) 
-			values ('$athlete', '$date', '$type', '5', '$r5', '$s5', '$d5', '')";
-		$result = $conn->query($sql);
-		$count++;
-	}
-	if ($d6 != "") {
-		$sql = "INSERT INTO splits (Athlete, Date, Type, `Split Number`, `Average Rate`, `500 Split`, Distance, `Total Time`) 
-			values ('$athlete', '$date', '$type', '6', '$r6', '$s6', '$d6', '')";
-		$result = $conn->query($sql);
-		$count++;
-	}
-	if ($d7 != "") {
-		$sql = "INSERT INTO splits (Athlete, Date, Type, `Split Number`, `Average Rate`, `500 Split`, Distance, `Total Time`) 
-			values ('$athlete', '$date', '$type', '7', '$r7', '$s7', '$d7', '')";
-		$result = $conn->query($sql);
-		$count++;
-	}
-	if ($d8 != "") {
-		$sql = "INSERT INTO splits (Athlete, Date, Type, `Split Number`, `Average Rate`, `500 Split`, Distance, `Total Time`) 
-			values ('$athlete', '$date', '$type', '8', '$r8', '$s8', '$d8', '')";
-		$result = $conn->query($sql);
-		$count++;
-	}
-	if ($d9 != "") {
-		$sql = "INSERT INTO splits (Athlete, Date, Type, `Split Number`, `Average Rate`, `500 Split`, Distance, `Total Time`) 
-			values ('$athlete', '$date', '$type', '9', '$r9', '$s9', '$d9', '')";
-		$result = $conn->query($sql);
-		$count++;
-	}
-	if ($d10 != "") {
-		$sql = "INSERT INTO splits (Athlete, Date, Type, `Split Number`, `Average Rate`, `500 Split`, Distance, `Total Time`) 
-			values ('$athlete', '$date', '$type', '10', '$r10', '$s10', '$d10', '')";
-		$result = $conn->query($sql);
-		$count++;
-	}
-	if ($d11 != "") {
-		$sql = "INSERT INTO splits (Athlete, Date, Type, `Split Number`, `Average Rate`, `500 Split`, Distance, `Total Time`) 
-			values ('$athlete', '$date', '$type', '11', '$r11', '$s11', '$d11', '')";
-		$result = $conn->query($sql);
-		$count++;
-	}
-	if ($d12 != "") {
-		$sql = "INSERT INTO splits (Athlete, Date, Type, `Split Number`, `Average Rate`, `500 Split`, Distance, `Total Time`) 
-			values ('$athlete', '$date', '$type', '12', '$r12', '$s12', '$d12', '')";
-		$result = $conn->query($sql);
-		$count++;
-	}
-	if ($d13 != "") {
-		$sql = "INSERT INTO splits (Athlete, Date, Type, `Split Number`, `Average Rate`, `500 Split`, Distance, `Total Time`) 
-			values ('$athlete', '$date', '$type', '13', '$r13', '$s13', '$d13', '')";
-		$result = $conn->query($sql);
-		$count++;
-	}
-	if ($d14 != "") {
-		$sql = "INSERT INTO splits (Athlete, Date, Type, `Split Number`, `Average Rate`, `500 Split`, Distance, `Total Time`) 
-			values ('$athlete', '$date', '$type', '14', '$r14', '$s14', '$d14', '')";
-		$result = $conn->query($sql);
-		$count++;
-	}
-	if ($d15 != "") {
-		$sql = "INSERT INTO splits (Athlete, Date, Type, `Split Number`, `Average Rate`, `500 Split`, Distance, `Total Time`) 
-			values ('$athlete', '$date', '$type', '15', '$r15', '$s15', '$d15', '')";
-		$result = $conn->query($sql);
-		$count++;
-	}
-	if ($d16 != "") {
-		$sql = "INSERT INTO splits (Athlete, Date, Type, `Split Number`, `Average Rate`, `500 Split`, Distance, `Total Time`) 
-			values ('$athlete', '$date', '$type', '16', '$r16', '$s16', '$d16', '')";
-		$result = $conn->query($sql);
-		$count++;
-	}
-	if ($d17 != "") {
-		$sql = "INSERT INTO splits (Athlete, Date, Type, `Split Number`, `Average Rate`, `500 Split`, Distance, `Total Time`) 
-			values ('$athlete', '$date', '$type', '17', '$r17', '$s17', '$d17', '')";
-		$result = $conn->query($sql);
-		$count++;
-	}
-	if ($d18 != "") {
-		$sql = "INSERT INTO splits (Athlete, Date, Type, `Split Number`, `Average Rate`, `500 Split`, Distance, `Total Time`) 
-			values ('$athlete', '$date', '$type', '18', '$r18', '$s18', '$d18', '')";
-		$result = $conn->query($sql);
-		$count++;
-	}
-	if ($d19 != "") {
-		$sql = "INSERT INTO splits (Athlete, Date, Type, `Split Number`, `Average Rate`, `500 Split`, Distance, `Total Time`) 
-			values ('$athlete', '$date', '$type', '19', '$r19', '$s19', '$d19', '')";
-		$result = $conn->query($sql);
-		$count++;
-	}
-	if ($d20 != "") {
-		$sql = "INSERT INTO splits (Athlete, Date, Type, `Split Number`, `Average Rate`, `500 Split`, Distance, `Total Time`) 
-			values ('$athlete', '$date', '$type', '20', '$r20', '$s20', '$d20', '')";
-		$result = $conn->query($sql);
-		$count++;
-	}
 	
 	//total time calc, total distance calc, average split calc, average rate calc
-	$totalDistance = $d1+$d2+$d3+$d4+$d5+$d6+$d7+$d8+$d9+$d10+$d11+$d12+$d13+$d14+$d15+$d16+$d17+$d18+$d19+$d20;
-	$avgRate = ($r1+$r2+$r3+$r4+$r5+$r6+$r7+$r8+$r9+$r10+$r11+$r12+$r13+$r14+$r15+$r16+$r17+$r18+$r19+$r20)/$count;
+	//$totalDistance = $d1+$d2+$d3+$d4+$d5+$d6+$d7+$d8+$d9+$d10+$d11+$d12+$d13+$d14+$d15+$d16+$d17+$d18+$d19+$d20;
+	//$avgRate = ($r1+$r2+$r3+$r4+$r5+$r6+$r7+$r8+$r9+$r10+$r11+$r12+$r13+$r14+$r15+$r16+$r17+$r18+$r19+$r20)/$count;
 	//Total Time
 	//Average Split
 	
