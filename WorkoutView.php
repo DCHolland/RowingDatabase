f808a4e81582677c33f979fe38774c73bc98e89b
DCHolland <Dcholla@okstate.edu>
2020-04-26 23:06:48 -0500

    Added workout view
    Now prints splits for individual workouts

--- WorkoutView.php
+++ WorkoutView.php
@@ -46,6 +46,42 @@
 </div>
   <h1>Workout View:</h1>
   <h2><?=$_POST['athlete'], " - ",$_POST['type'], " - ", $_POST['date'];?></h2>
+  <table class="table table-striped">
+  <thead><th align="left">Date</th><th align="left">Athlete</th><th align="left">Type</th><th align="left">Split Number</th><th align="left">Average Rate</th><th align="left">500 Split</th><th align="left">Distance</th><th align="left">Total Time</th></thead>
+  <tbody>
+  <?php
+  $servername = "localhost";
+  $username = "micah.swedberg";
+  $password = "password";
+  $dbname = "okstatecrewergtracker";
 
+  // Create connection
+  $conn = new mysqli($servername, $username, $password, $dbname);
+  // Check connection
+  if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+  }
+  $name =$_POST['athlete'];
+  $type =$_POST['type'];
+  $date =$_POST['date'];
+  $sql = "SELECT * FROM `splits` WHERE Type LIKE '%{$type}%' AND Date LIKE '%{$date}%' AND Athlete LIKE '%{$name}%'";
+  $result = mysqli_query($conn, $sql);
+while($row = mysqli_fetch_assoc($result)) {
+  echo "<tr>";
+  echo "<td>". $row["Date"]."</td>";
+  echo "<td>".$row["Athlete"]."</td>";
+  echo "<td>" . $row["Type"]. "</td>";
+  echo "<td>" . $row["Split Number"]. "</td>";
+  echo "<td>" . $row["Average Rate"]. "</td>";
+  echo "<td>" . $row["500 Split"]. "</td>";
+  echo "<td>" . $row["Distance"]. "</td>";
+  echo "<td>" . $row["Total Time"]. "</td>";
+
+
+  echo "</tr>";
+}
+  ?>
+</tbody>
+</table>
 </body>
 </html>
